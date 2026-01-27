58aa40d0da2b07305c2e5dced2bfb2d7c8767abd
FWA-Khoerul.MUTAQIN <khoerul.mutaqin@falinwa.com>
2026-01-27 11:22:22 +0700

    Create sale_order.py

---
+++ models/sale_order.py
@@ -0,0 +1,29 @@
+from odoo import models, fields, api, _
+import logging
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+      
+    x_discount = fields.Monetary(string="Discount", store=True, compute='_compute_x_discount')
+    x_amount_untaxed = fields.Monetary(string="Untaxed Amount", store=True, compute='_compute_x_amount_untaxed')
+    
+    @api.depends('order_line.price_subtotal', 'currency_id', 'company_id', 'payment_term_id')
+    def _compute_x_discount(self):
+        x_discount = 0
+        for order in self:
+            discount_line = self.order_line.filtered(lambda x: x.price_subtotal > 0)
+            #discount line 
+            for line in discount_line:
+                x_discount += line.price_subtotal
+            order['x_discount'] = x_discount
+
+    @api.depends('order_line.price_subtotal', 'currency_id', 'company_id', 'payment_term_id')
+    def _compute_x_amount_untaxed(self):
+        x_amount_untaxed = 0
+        for order in self:
+            untaxed_line = self.order_line.filtered(lambda x: x.price_subtotal > 0)
+            #untaxed line 
+            for line in untaxed_line:
+                x_amount_untaxed += line.price_subtotal
+            order['x_amount_untaxed'] = x_amount_untaxed
